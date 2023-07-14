494a391da282958b709bad598916e0a629a53293
thanhnb1101 <thanhnb1101@gmail.com>
2023-07-14 09:48:06 +0700

    init backend

---
+++ backend-heroes/src/main/java/com/demo/heroes/BackendHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.demo.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHeroesApplication.class, args);
+	}
+
+}
---
+++ backend-heroes/src/test/java/com/demo/heroes/BackendHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
